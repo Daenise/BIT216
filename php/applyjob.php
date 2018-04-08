dda97b1dcbb9b54ab8bdde9fe61e718fd176446a
Yap Yuen Peng <yappeng95@gmail.com>
2018-04-09 00:19:09 +0800

    apply job php

---
+++ php/applyjob.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include '../php/dbConnection.php';
+
+if (isset($_POST['applyJob'])) {
+  $id = $_POST['jobID'];
+  $email =  $_SESSION['email'];
+  $status = "Pending";
+
+  $sql = "INSERT INTO  application (jobID,partTimerEmail,jobStatus)
+          VALUES ('$id','$email','$status')";
+  mysqli_query($connection, $sql);
+}
+
+?>
