fcb397ec7b943e24f64ecd545c704d2e1ee04db0
Daenise <31722262+Daenise@users.noreply.github.com>
2018-04-09 00:38:27 +0800

    checkError

--- php/applyjob.php
+++ php/applyjob.php
@@ -7,11 +7,12 @@ if (isset($_POST['applyJob'])) {
   $email =  $_SESSION['email'];
   $status = "Pending";
 
-  $sql = "INSERT INTO  application (jobID,partTimerEmail,jobStatus)
+  $sql = "INSERT INTO application (jobID,partTimerEmail,jobStatus)
           VALUES ('$id','$email','$status')";
   mysqli_query($connection, $sql);
 
-  header('location: ../webpage/searchJob.php');
+  //header('location: ../webpage/searchJob.php');
+  mysqli_error($con);
 
 }
 
